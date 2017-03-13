8b7b227219566e4dfa727036d180b679a89bab86
Creder <creder67@gmail.com>
2017-03-13 11:17:37 +0400

    ---

--- app/controllers/users/tags_controller.rb
+++
@@ -1,9 +0,0 @@
-class TagsController < ApplicationController
-
-	def index
-		@tags = Tag.order(:name)
-		respond_to do |format|
-			format.html
-			format.json { render json: @tags.tokens(params[:q]) }
-	end
-end
